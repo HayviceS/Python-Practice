c3f245a76745809a29a92753714461710884116a
Sienna Hayvice <159972064+HayviceS@users.noreply.github.com>
2025-05-08 10:27:52 +1200

    Update milestone_1_code.py

--- milestone_1/milestone_1_code.py
+++ milestone_1/milestone_1_code.py
@@ -1,10 +1,12 @@
 #Collecting info
 name = input("What is your name?")
-age = input("How old are you?")
+age = int(input("How old are you?"))
 color = input("What's your favorite color?")
 
 #Doubling age
-double_age = ("{} * 2".format(age))
+double_age = 2
+double_age *= age
+
 
 #Printing info
 print("Hi {}, you are {} when you double your age and your favorite color is {}.".format(name, double_age, color))
