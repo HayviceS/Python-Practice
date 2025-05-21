bc8a52af20b7db15dea7c972518660adb0ac028a
Sienna Hayvice <159972064+HayviceS@users.noreply.github.com>
2025-05-22 10:31:05 +1200

    code

--- milestone_2/milestone_2_code.py
+++ milestone_2/milestone_2_code.py
@@ -1,19 +1,30 @@
+from datetime import datetime, timedelta
 #Getting info on the user
 print("The information you put in will be stored, if you do not want this do not answer the questions.")
-waking_up = int(input("What time do you wake up on a normal day: "))
-age = int(input("How old are you: "))
+waking_up = input("What time do you wake up on a normal day?(HH:MM in 24-hour format): ")
+age = int(input("How old are you? "))
 
+wake_time = datetime.strptime(waking_up, "%H:%M")
+
+#Calculating
 if age > 0 and age <= 2:
-    print("Are you allowed to be online? Oh well, you should be getting 12 - 17 hours of sleep per day, the younger you are the more you should sleep!")
+    min_sleep, max_sleep = 11, 14
 elif age >= 3 and age <= 5:
-    print("Your still growing and need a lot of sleep, 10 - 13 hours a day to be specific.")
+    min_sleep, max_sleep = 10, 13
 elif age >= 6 and age <= 13:
-    print("I know you probably want to stay up late, but you should be getting at least 9 hours of sleep and a maximum of 12 hours!")
+    min_sleep, max_sleep = 9, 12
 elif age >= 14 and age <= 18:
-    print("Your teen years are hard but a good night sleep can help with that! Try getting 8 - 10 hours of sleep a night")
+   min_sleep, max_sleep = 8, 10
 elif age >= 19 and age <= 125:
-    print("To be a healthy adult you need to be getting 7 - 9 hours of sleep. This ")
-elif age <= 126:
-    print("I find that hard to believe, but you should still be getting 7 - 9 hours of sleep a night.")
+    min_sleep, max_sleep = 7, 9
+elif age >= 126:
+    print("I find that hard to believe, but you should be getting 7 - 9 hours of sleep a night.")
+    min_sleep, max_sleep = 7, 9
 else:
     print("You can't be that old!")
+
+earliest_bedtime = (wake_time - timedelta(hours=max_sleep)).time()
+latest_bedtime = (wake_time - timedelta(hours=min_sleep)).time()
+
+#Output 
+print(f"Based on the information given, your age ({age}) and the time you wake up ({waking_up}) you should go to bed between {earliest_bedtime.strftime('%H:%M')} and {latest_bedtime.strftime('%H:%M')}.")
